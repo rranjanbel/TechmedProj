d16c527219078457c3a13cd606d6518f10783ba1
Rajesh Kumar Ranjan <rajeshmca2005@gmail.com>
2022-03-31 18:26:21 +0530

    Jwt and Looger update

---
+++ TechMed.API/JwtInfra/JwtRefreshTokenCache.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace TechMedAPI.JwtInfra
+{
+    public class JwtRefreshTokenCache : IHostedService, IDisposable
+    {
+        private Timer _timer;
+        private readonly IJwtAuthManager _jwtAuthManager;
+
+#pragma warning disable CS8618 // Non-nullable field '_timer' must contain a non-null value when exiting constructor. Consider declaring the field as nullable.
+        public JwtRefreshTokenCache(IJwtAuthManager jwtAuthManager)
+#pragma warning restore CS8618 // Non-nullable field '_timer' must contain a non-null value when exiting constructor. Consider declaring the field as nullable.
+        {
+            _jwtAuthManager = jwtAuthManager;
+        }
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            // remove expired refresh tokens from cache every minute
+#pragma warning disable CS8622 // Nullability of reference types in type of parameter 'state' of 'void JwtRefreshTokenCache.DoWork(object state)' doesn't match the target delegate 'TimerCallback' (possibly because of nullability attributes).
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
+#pragma warning restore CS8622 // Nullability of reference types in type of parameter 'state' of 'void JwtRefreshTokenCache.DoWork(object state)' doesn't match the target delegate 'TimerCallback' (possibly because of nullability attributes).
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            _jwtAuthManager.RemoveExpiredRefreshTokens(DateTime.Now);
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
--- TechMed.API/Program.cs
+++ TechMed.API/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using System.Text;
+using TechMedAPI.JwtInfra;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,19 +13,79 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var jwtTokenConfig = builder.Configuration.GetSection("jwtTokenConfig").Get<JwtTokenConfig>();
+builder.Services.AddSingleton(jwtTokenConfig);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = true;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = jwtTokenConfig.Issuer,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtTokenConfig.Secret)),
+        ValidAudience = jwtTokenConfig.Audience,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.FromMinutes(1)
+    };
+});
+builder.Services.AddSingleton<IJwtAuthManager, JwtAuthManager>();
+builder.Services.AddHostedService<JwtRefreshTokenCache>();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "TechMed API", Version = "v1" });
+
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "JWT Authentication",
+        Description = "Enter JWT Bearer token **_only_**",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer", // must be lower case
+        BearerFormat = "JWT",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+    c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {securityScheme, new string[] { }}
+                });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); });
+});
 var app = builder.Build();
-
+var log = new LoggerFactory();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
+app.UseRouting();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
+
+log.AddSerilog();
 
 app.MapControllers();
 
