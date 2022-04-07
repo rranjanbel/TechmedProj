f260eb31e3e3b0e0864b05139dcd00d6ff54fbec
Rajesh Roshan <rajesh.roshan3108@outlook.com>
2022-04-07 14:47:23 +0530

    Create MV for PatientCase and Setting Master.

--- TechMed.BL/CommanClassesAndFunctions/SettingMaster.cs
+++ TechMed.BL/CommanClassesAndFunctions/SettingMaster.cs
@@ -1,21 +1,75 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechMed.BL.Adapters;
 using TechMed.DL.Models;
 
 namespace TechMed.BL.CommanClassesAndFunctions
 {
-    public class SettingMaster
-    {
-        public TeleMedecineContext _teleMedecineContext;
+    public class SettingMaster 
+    {        
+        private readonly TeleMedecineContext _teleMedecineContext;
         public SettingMaster(TeleMedecineContext teleMedecineContext)
         {
             this._teleMedecineContext = teleMedecineContext;
         }
-        public int GetPatientNumber()
-        {           
+        public long GetPatientNumber()
+        {
+            Setting setting = new Setting();
+            Int64 currentNo = 0;
+            var value = _teleMedecineContext.Settings.Select(a => a.PatientNumber);
+            if (value == null)
+            {
+                currentNo = Convert.ToInt64(value);
+                var setModel = _teleMedecineContext.Settings.FirstOrDefault();
+                if (setModel != null)
+                {
+                    setting = (Setting)setModel;
+                    setting.PatientNumber = currentNo + 1;
+                }
+                try
+                {
+                    this._teleMedecineContext.Entry(setting).State = EntityState.Modified;
+                    var result = this._teleMedecineContext.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    throw;
+                }
+                return currentNo + 1;
+            }
+            return 0;
+        }
+        public long GetCaseFileNumber()
+        {
+            Setting setting = new Setting();
+            Int64 currentNo = 0;
+            var value = _teleMedecineContext.Settings.Select(a => a.CaseFileNumber);
+            if (value == null)
+            {
+                currentNo = Convert.ToInt64(value);
+                var setModel = _teleMedecineContext.Settings.FirstOrDefault();
+                if (setModel != null)
+                {
+                    setting = (Setting)setModel;
+                    setting.CaseFileNumber = currentNo+1;
+                }
+                try
+                {
+                    this._teleMedecineContext.Entry(setting).State = EntityState.Modified;
+                    var result = this._teleMedecineContext.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    throw;
+                }
+                return currentNo + 1;
+            }
             return 0;
         }
     }
---
+++ TechMed.BL/DTOMaster/PatientCaseDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechMed.BL.DTOMaster
+{
+    public class PatientCaseDTO
+    {
+        public long Id { get; set; }
+        public int PatientId { get; set; }
+        public string CaseFileNumber { get; set; } = null!;
+        public string CaseHeading { get; set; } = null!;
+        public string? Observation { get; set; }
+        public string? Allergies { get; set; }
+        public string? FamilyHistory { get; set; }
+        public string? Diagnosis { get; set; }
+        public string? Instruction { get; set; }
+        public string? Test { get; set; }
+        public int CreatedBy { get; set; }     
+        public int? UpdatedBy { get; set; }       
+    }
+}
