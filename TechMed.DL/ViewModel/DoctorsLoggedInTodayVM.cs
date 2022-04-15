3a450b08ba6c64ed70164f867239e1349b75eeef
bbsjitendra <jitendra.yadav@bellurbis.com>
2022-04-15 12:57:27 +0530

    added dashboard repo..

---
+++ TechMed.DL/ViewModel/DoctorsLoggedInTodayVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechMed.DL.ViewModel
+{
+    public class DoctorsLoggedInTodayVM
+    {
+        public int? ZoneID { get; set; }
+        public int? ClusterID { get; set; }
+    }
+}
