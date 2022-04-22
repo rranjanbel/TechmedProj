becc8da90d0564d709fb0bc9ce09a5097dd83487
bbsjitendra <jitendra.yadav@bellurbis.com>
2022-04-22 18:57:16 +0530

    added new tables

---
+++ TechMed.DL/Models/SPResultGetPatientDetails.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechMed.DL.Models
+{
+    [Keyless]
+    public class SPResultGetPatientDetails
+    {
+        public string PHCName { get; set; }
+        public string PHCAddress { get; set; }
+        public string MOName { get; set; }
+        public string PhoneNo { get; set; }
+        public string MailID { get; set; }
+        public string Cluster { get; set; }
+        public string Zone { get; set; }
+        public string PatientName { get; set; }
+        public string PatientCreatedBy { get; set; }
+        public string Docter { get; set; }
+       
+    }
+}
