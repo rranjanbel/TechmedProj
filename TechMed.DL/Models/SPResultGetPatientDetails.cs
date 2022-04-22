6b43e258273b6b4100aa2d2a33a1340086aed5a7
Rajesh Roshan <rajesh.roshan3108@outlook.com>
2022-04-22 19:04:24 +0530

    commit changes

--- TechMed.DL/Models/SPResultGetPatientDetails.cs
+++ TechMed.DL/Models/SPResultGetPatientDetails.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,19 @@ using System.Threading.Tasks;
 
 namespace TechMed.DL.Models
 {
+    [Keyless]
     public class SPResultGetPatientDetails
     {
-        public int PatientID { get; set; }
+        public string PHCName { get; set; }
+        public string PHCAddress { get; set; }
+        public string MOName { get; set; }
+        public string PhoneNo { get; set; }
+        public string MailID { get; set; }
+        public string Cluster { get; set; }
+        public string Zone { get; set; }
+        public string PatientName { get; set; }
+        public string PatientCreatedBy { get; set; }
+        public string Docter { get; set; }
+       
     }
 }
