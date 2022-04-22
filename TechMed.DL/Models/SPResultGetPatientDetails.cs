38c1d854d575d7e7bef3be1f9b1aee12ab46354e
Rajesh Roshan <rajesh.roshan3108@outlook.com>
2022-04-22 15:15:42 +0530

    commit

---
+++ TechMed.DL/Models/SPResultGetPatientDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechMed.DL.Models
+{
+    public class SPResultGetPatientDetails
+    {
+        public int PatientID { get; set; }
+    }
+}
