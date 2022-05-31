3923d1ef6315cb4f9128543b83433ffde66f996e
Rajesh Roshan <rajesh.roshan3108@outlook.com>
2022-05-31 09:39:42 +0530

    Coomit spoke wep api changes

---
+++ TechMed.BL/DTOMaster/SpokeMaintenanceDTO.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechMed.BL.DTOMaster
+{
+    public class SpokeMaintenanceDTO
+    {
+        public int Phcid { get; set; }      
+        public IFormFile file { get; set; }
+    }
+}
