f2c423660381195e53f565ef9490ac0258fa0819
bbsjitendra <jitendra.yadav@bellurbis.com>
2022-07-19 10:05:05 +0530

    added Exception in GeneratePdfReport function ..

--- TechMed.BL/Repository/BaseClasses/DoctorRepository.cs
+++ TechMed.BL/Repository/BaseClasses/DoctorRepository.cs
@@ -513,7 +513,7 @@ namespace TechMed.BL.Repository.BaseClasses
                     }
                     catch (Exception ex)
                     {
-                        throw;
+                        _logger.LogError("Exception in GeneratePdfReport service " + ex);
                     }
 
 
