80eacfc5e5dba8effdf7142fd11ce6720f24167f
Jitendra <yjitendra72@live.com>
2022-07-27 15:09:18 +0530

    Update date time to Indian Time Zone..

--- HealthWorkerService/Help.cs
+++ HealthWorkerService/Help.cs
@@ -10,8 +10,10 @@ namespace HealthWorkerService
     {
         //Uninstall service
         //sc.exe delete "TestService"
+        //sc.exe delete "ServerHealthService"
 
         //install service
         //sc create TestService BinPath=C:\Users\ADMIN\source\repos\Dashboard_Changes\TechmedProj\HealthWorkerService\bin\Release\net6.0\publish\HealthWorkerService.exe
+        //sc create ServerHealthService BinPath=C:\Users\Jitendra\source\repos\rranjanbel\TechmedProj\HealthWorkerService\bin\Release\net6.0\publish\HealthWorkerService.exe
     }
 }
