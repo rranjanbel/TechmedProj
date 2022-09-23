aedd88c848ca7def90e502920e43c60f698ca2b0
bbsjitendra <jitendra.yadav@bellurbis.com>
2022-09-23 11:39:58 +0530

    addded clear queue function

---
+++ TechMed.BL/ZoomAPI/Model/WebHookEndMeetingResponse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechMed.BL.ZoomAPI.Model
+{
+    internal class WebHookEndMeetingResponse
+    {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+        public class Object
+        {
+            public long duration { get; set; }
+            public DateTime start_time { get; set; }
+            public string timezone { get; set; }
+            public DateTime end_time { get; set; }
+            public string topic { get; set; }
+            public string id { get; set; }
+            public int type { get; set; }
+            public string uuid { get; set; }
+            public string host_id { get; set; }
+        }
+
+        public class Payload
+        {
+            public string account_id { get; set; }
+            public Object @object { get; set; }
+        }
+
+        public class WebHookEndMeetingModel
+        {
+            public string @event { get; set; }
+            public Payload payload { get; set; }
+            public long event_ts { get; set; }
+        }
+
+    }
+}
--- TechMed.BL/ZoomAPI/Service/ZoomWebhook.cs
+++ TechMed.BL/ZoomAPI/Service/ZoomWebhook.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using TechMed.BL.CommanClassesAndFunctions;
+using TechMed.BL.Repository.Interfaces;
 using TechMed.BL.ZoomAPI.Model;
 using TechMed.DL.Models;
 
@@ -13,9 +15,11 @@ namespace TechMed.BL.ZoomAPI.Service
     public class ZoomWebhook : IZoomWebhook
     {
         readonly TeleMedecineContext _teleMedecineContext;
-        public ZoomWebhook(TeleMedecineContext teleMedecineContext)
+        readonly ITwilioMeetingRepository _twilioMeetingRepository;
+        public ZoomWebhook(TeleMedecineContext teleMedecineContext, ITwilioMeetingRepository twilioMeetingRepository)
         {
             _teleMedecineContext = teleMedecineContext;
+            _twilioMeetingRepository = twilioMeetingRepository;
         }
         public async Task<bool> ZoomWebhookService(string value)
         {
@@ -27,6 +31,36 @@ namespace TechMed.BL.ZoomAPI.Service
             var myDeserializedClass = JsonSerializer.Deserialize<ZoomWebHookResponseModel>(value);
             zoomWebHookEvent.EventName = myDeserializedClass.@event;
             await _teleMedecineContext.SaveChangesAsync();
+            if (myDeserializedClass.@event.ToLower() == "meeting.ended")
+            {
+                //clear queue
+                var response = JsonSerializer.Deserialize<WebHookEndMeetingResponse.WebHookEndMeetingModel>(value);
+                if (response != null)
+                {
+                    if (response.payload.@object != null)
+                    {
+                        TwilioMeetingRoomInfo twilioMeetingRoomInfo = await _teleMedecineContext.TwilioMeetingRoomInfos.AsNoTracking().FirstOrDefaultAsync(a => a.MeetingSid == response.payload.@object.id);
+                        if (twilioMeetingRoomInfo != null)
+                        {
+                            if (twilioMeetingRoomInfo.IsClosed == false)
+                            {
+                                bool result = await _twilioMeetingRepository.SetMeetingRoomClosed(response.payload.@object.id, true);
+
+                            }
+                        }
+                    }
+                }
+
+            }
+            if (myDeserializedClass.@event.ToLower() == "meeting.participant_left")
+            {
+                //end meeting
+            }
+            if (myDeserializedClass.@event.ToLower() == "recording.stopped")
+            {
+                //clear queue
+            }
+
             return true;
         }
     }
